2ade3f402fcbc1ba212f111fe2ad052d03db23fb
Arhangel64 <arhangel64@bk.ru>
2016-06-01 08:26:46 +0300

    temp, keep doing grid layout

--- lorgar/views/gridLayout.js
+++ lorgar/views/gridLayout.js
@@ -33,64 +33,160 @@
                     this._lay[row].push(false);
                 }
                 this._lay[row][col] = child;
+                
+                this._recountLimits();
                 if (this._w !== undefined && this._h !== undefined) {
                     this.refreshLay();
                 }
             },
-            "refreshLay": function() {
+            "_recountLimits": function() {
                 this._cols = [];
                 this._rows = [];
                 
                 for (var i = 0; i < this._lay.length; ++i) {
                     for (var j = 0; j < this._lay[i].length) {
                         var e = this._lay[i][j];
+                        while (!this._cols[j]) {
+                            this._cols.push({});
+                        }
+                        while (!this._rows[i]) {
+                            this._rows.push({});
+                        }
                         if (e) {
-                            var colW = this._cols[j] || 0;
-                            var rowH = this._rows[i] || 0;
-                            this._cols[j] = Math.max(colW, e.options.minWidth);
-                            this._rows[i] = Math.max(rowH, e.options.minHeight);
+                            var colMinW = this._cols[j].min || 0;
+                            var rowMinH = this._rows[i].min || 0;
+                            var colMaxW = this._cols[j].max === undefined ? Infinity : this._cols[j].max;
+                            var rowMaxH = this._rows[i].max === undefined ? Infinity : this._rows[i].max;
+                            
+                            this._cols[j].min = Math.max(colMinW, e.options.minWidth);
+                            this._rows[i].min = Math.max(rowMinH, e.options.minHeight);
+                            this._cols[j].max = Math.min(colMaxW, e.options.maxWidth);
+                            this._rows[i].max = Math.min(rowMaxH, e.options.maxHeight);
                         } else {
-                            this._cols[j] = this._cols[j] || 0;
-                            this._rows[i] = this._rows[i] || 0;
+                            this._cols[j].min = this._cols[j].min || 0;
+                            this._rows[i].min = this._rows[i].min || 0;
+                            this._cols[j].max = this._cols[j].max || 0;
+                            this._rows[i].max = this._rows[i].max || 0;
                         }
                     }
                 }
-                
-                var totalW = 0;
-                var totalH = 0;
+            },
+            "refreshLay": function() {
+                var totalMaxW = 0;
+                var totalMaxH = 0;
+                var totalMinW = 0;
+                var totalMinH = 0;
                 
                 for (i = 0; i < this._cols.length; ++i) {
-                    totalW += this._cols[i];
+                    totalMaxW += this._cols[i].max;
+                    totalMinW += this._cols[i].min
                 }
                 for (i = 0; i < this._rows.length; ++i) {
-                    totalH += this._rows[i];
+                    totalMaxH += this._rows[i].max;
+                    totalMinH += this._rows[i].min;
                 }
                 
-                var kW = this._w / totalW;
-                var kH = this._h / totalH;
-                var shiftW = 0;
-                var shiftH = 0;
-                
-                for (i = 0; i < this._cols.length; ++i) {
-                    this._cols[i] = this._cols[i] * kW;
-                    shiftW += this._cols[i];
-                    for (j = 0; j < this._lay.length; ++j) {
-                        e = this._lay[j][i];
-                        if (e) {
-                            e._e.style.left = shiftW + "px";
+                if (this._w <= totalMinW || this._w >= totalMaxW) {
+                    var kW;
+                    var keyW;
+                    if (this._w <= totalMinW) {
+                        kW = this._w / totalMinW;
+                        keyW = "min";
+                    } else {
+                        kW = this._w / totalMaxW;
+                        keyW = "max";
+                    }
+                    
+                    for (i = 0; i < this._cols.length; ++i) {
+                        this._cols[i].cur = this._cols[i][keyW] * kW;
+                    }
+                } else {
+                    var restW = this._w - totalMinW;
+                    var minDotationW = Infinity;
+                    var candidatesW = [];
+                    for (i = 0; i < this._cols.length; ++i) {
+                        this._cols[i].cur = this._cols[i].min;
+                        
+                        if (this._cols[i].cur < this._cols[i].max) {
+                            var donationW = this._cols[i].max - this._cols[i].cur;
+                            this._cols[i].don = donationW;
+                            candidatesW.push(this._cols[i]);
+                            Math.min(minDotationW, donationW);
+                        }
+                        
+                    }
+                    
+                    candidatesW.sort(GridLayout._candidatesSort);
+                    
+                    while (restW) {
+                        var portionW = restW / candidatesW.length;
+                        if (portionW <= minDotationW) {
+                            for (i = 0; i < candidatesW.length; ++i) {
+                                candidatesW[i].cur += portionW;
+                                delete candidatesW[i].don
+                            }
+                            restW = 0;
+                        } else {
+                            candidatesW[0].cur += minDotationW;
+                            delete candidatesW[0].don;
+                            candidatesW.shift();
+                            restW -=minDotationW;
+                            minDotationW = candidatesW[0].don;
                         }
                     }
                 }
-                for (i = 0; i < this._rows.length; ++i) {
-                    this._rows[i] = this._rows[i] * kH;
-                    shiftH += this._rows[i];
-                    for (j = 0; j < this._lay[i].length; ++j) {
-                        e = this._lay[i][j];
-                        if (e) {
-                            e._e.style.top = shiftH + "px";
+                
+                if (this._h <= totalMinH || this._w >= totalMaxH) {
+                    var kH;
+                    var keyH;
+                    if (this._h <= totalMinH) {
+                        kH = this._h / totalMinH;
+                        keyH = "min";
+                    } else {
+                        kH = this._h / totalMaxH;
+                        keyH = "max";
+                    }
+                    
+                    for (i = 0; i < this._rows.length; ++i) {
+                        this._rows[i].cur = this._rows[i][keyH] * kH;
+                    }
+                } else {
+                    var restH = this._h - totalMinH;
+                    var minDotationH = Infinity;
+                    var candidatesH = [];
+                    for (i = 0; i < this._rows.length; ++i) {
+                        this._rows[i].cur = this._rows[i].min;
+                        
+                        if (this._rows[i].cur < this._rows[i].max) {
+                            var donationH = this._rows[i].max - this._rows[i].cur;
+                            this._rows[i].don = donationH;
+                            candidatesH.push(this._rows[i]);
+                            Math.min(minDotationH, donationH);
+                        }
+                        
+                    }
+                    candidatesH.sort(GridLayout._candidatesSort);
+                    while (restH) {
+                        var portionH = restH / candidatesH.length;
+                        if (portionH <= minDotationH) {
+                            for (i = 0; i < candidatesH.length; ++i) {
+                                candidatesH[i].cur += portionH;
+                                delete candidatesH[i].don
+                            }
+                            restH = 0;
+                        } else {
+                            candidatesH[0].cur += minDotationH;
+                            delete candidatesH[0].don;
+                            candidatesH.shift();
+                            restH -=minDotationH;
+                            minDotationH = candidatesH[0].don;
                         }
                     }
                 }
+                this._positionElements();
+            },
+            "_positionElements": function() {
+                
             },
             "setSize": function(w, h) {
                 this._w = this.constrainWidth(w);
@@ -102,6 +198,10 @@
                 this.refreshLay();
             }
         });
+        
+        GridLayout._candidatesSort = function(a, b) {
+            return a.don - b.don;
+        }
         
         return GridLayout;
     });
